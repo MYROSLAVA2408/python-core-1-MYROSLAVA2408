e66152827f0cabeaca78d4f379ef5f1aab6a46bb
MYROSLAVA2408 <mira53968@gmail.com>
2022-02-18 16:07:38 +0300

    N10

--- src/mean.py
+++ src/mean.py
@@ -1,3 +1,11 @@
 def mean(number):
-  # Type your code
-  return mean
+  sum = 0
+  while number > 0:
+    sum += number % 10
+    number = number//10
+  return sum // 2
+
+
+print(mean(42))
+print(mean(64))
+print(mean(79))
