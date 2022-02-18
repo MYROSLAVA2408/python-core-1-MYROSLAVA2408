5b04e13797a61f5ba7212c4d498cc9bf5ec39f1f
MYROSLAVA2408 <mira53968@gmail.com>
2022-02-18 15:33:02 +0300

    N9sum

--- src/sum_even_nums_in_range.py
+++ src/sum_even_nums_in_range.py
@@ -1,3 +1,15 @@
 def sum_even_nums_in_range(start, stop):
-  # Type your code
-  return
+
+  count = 0
+  for x in range(start, stop):
+            if x % 2 == 0:
+              count += x
+  return count
+
+
+print(sum_even_nums_in_range(4, 13))
+print(sum_even_nums_in_range(1, 9))
+
+
+
+
